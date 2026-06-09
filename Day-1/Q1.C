424ea1cc0a7dc4aa5bf56ce90ef0f00cb82c5bf3
siddharth singh <its05siddharth@gmail.com>
2026-06-09 21:02:45 +0530

    day 1 assignment is uploaded

--- Day-1/Q1.C
+++ Day-1/Q1.C
@@ -4,7 +4,10 @@ int main()
     int n ,sum = 0;
     printf("Enter a number: ");
     scanf("%d", &n);
+    for(int i=0;i<=n;i++){
+        sum=sum+i;
+    }
+    printf("Sum of numbers from 0 to %d is: %d", n, sum);
 
     return 0;
-}
 }
---
+++ Day-1/Q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("Multiplication table of %d:\n", num);
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", num * i);
+    }
+    
+    return 0;
+}
